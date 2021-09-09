331a877c634fb22b55cae4de573514fe62daaab4
duoli <zhaody901@126.com>
2021-09-10 07:08:50 +0800

    index config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -39,7 +39,7 @@ module.exports = {
 			"@vuepress/docsearch",
 			{
 				apiKey: "9eca016e40335ee789f9a73253c13be1",
-				indexName: "poppy",
+				indexName: "lang",
 				appId: "LD89A1JN8Y",
 				locales: {
 					"/": {
